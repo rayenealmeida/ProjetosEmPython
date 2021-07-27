23212c1f6ce5d90c15cb6856bf4ddb21fad6a59b
rayene <almeidarayene@gmail.com>
2021-07-27 17:09:04 -0300

    Jogo de Aventura

---
+++ JogoDeAventura.py
@@ -0,0 +1,7 @@
+# Jogo de Aventura 
+# Um jogo de decisões onde eu terei que criar vários finais diferentes baseados nas respostas que forem dadas
+# chegar em finais diferentes na minha historia de acordo com as respostas que eu passe para o programa
+
+class jogoDeAventura:
+    def __init__(self):
+        self.pergunta1 = '' 
