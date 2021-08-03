7fd017a92c49ff62cc6cb975ff371e5c22e377ec
rayene <almeidarayene@gmail.com>
2021-08-03 15:07:08 -0300

    simulador de dado gui

--- SimuladorDeDadoGUI.py
+++ SimuladorDeDadoGUI.py
@@ -1,6 +1,7 @@
 import random
 import PySimpleGUI as sg
 
+
 class SimuladorDeDado:
     def __init__(self):
         self.valor_minimo = 1
@@ -17,16 +18,16 @@ class SimuladorDeDado:
 # Ler os valores da tela
         self.eventos, self.valores = self.janela.Read()
 # fazer alguma coisa com esses valores
-        
-        try:
-            if self.eventos == 'sim' or self.eventos == 's':
-                self.GerarValorDoDado()
-            elif self.eventos == 'não' or self.eventos == 'n':
-                print('Agradecemos a sua participação')
-            else:
-                print('Favor digitar sim ou não')
-        except:
-              print('Ocorreu um erro ao receber sua resposta')
+        while True:
+            try:
+                if self.eventos == 'sim' or self.eventos == 's':
+                    self.GerarValorDoDado()
+                elif self.eventos == 'não' or self.eventos == 'n':
+                    print('Agradecemos a sua participação')
+                else:
+                    print('Favor digitar sim ou não')
+            except:
+                print('Ocorreu um erro ao receber sua resposta')
 
     def GerarValorDoDado(self):
         print(random.randint(self.valor_minimo,self.valor_maximo))
