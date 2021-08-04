b98f3eb90949f8ee695831e42ded4ac700093374
rayene <almeidarayene@gmail.com>
2021-08-04 14:52:10 -0300

    Decida por mim GUI

--- DecidaPorMimGUI.py
+++ DecidaPorMimGUI.py
@@ -5,29 +5,28 @@ class DecidaPorMim:
     def __init__(self):
         self.respostas = [
             'Com certeza, você deve fazer isso!',
-            'Não sei, você que sabe',
-            'Não faz isso não!',
+            'Não sei',
+            'Não faça isso!',
             'Acho que está na hora certa!'
         ]
+
     def iniciar(self):
         # layout
         layout = [
-            [sg.Text('Faça sua pergunta')],
+            [sg.Text('Faça sua pergunta:')],
             [sg.Input()],
-            [sg.Output(size=(20,10))],
+            [sg.Output(size=(50,10))],
             [sg.Button('Decida por mim')]
         ]
         #Criar a janela
-        self.janela = sg.Window('Decida por mim!')
+        self.janela = sg.Window('Decida por mim!',layout=layout)
+        while True:
         # ler valores
-        self.eventos, self.valores = self.janela.Read()
-        # Fazer alguma coisa com os valores
-        if self.eventos == 'Decida por mim':
-            print(random.choice(self.respostas))
-            self.janela.Close()
+            self.eventos, self.valores = self.janela.Read()
+            # Fazer alguma coisa com os valores
+            if self.eventos == 'Decida por mim':
+                print(random.choice(self.respostas))
 
-        input('Faça sua pergunta: ')
-        print(random.choice(self.respostas))
 
 decida = DecidaPorMim() 
 decida.iniciar()
